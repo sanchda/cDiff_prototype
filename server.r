137ec2dd8b254125f0a62c1a4c300308f126deda
David Sanchez <sanchda@gmail.com>
2015-06-26 14:31:12 -0400

    Fix adj generation; new display options

--- server.r
+++ server.r
@@ -66,7 +66,6 @@ patDf     = patDf[ !((1:nrow(patDf)) %in% replaceIdx),];  # Throw out the ones w
 # Addicted to the apply idiom, so create a function to thread over the rows.
 # Should be faster than just iterating through rows with for
 uncombinePatientNames = function(thisRow) {
-  print( thisRow );
   thesePats  = unlist(strsplit(thisRow[1], ","));
   thesePats  = gsub("^\\s+","",thesePats);
   thesePats  = gsub("\\s+$","",thesePats);
@@ -75,16 +74,18 @@ uncombinePatientNames = function(thisRow) {
 
 # Result is a list of data frames, stitch them together with a do.call to rbind
 answerList = apply( workingDf, 1, uncombinePatientNames );
-bufferDf = do.call(rbind, thisAnswer);
+bufferDf = do.call(rbind, answerList);
 patDf = rbind(patDf, bufferDf);
 rownames(patDf) = NULL;
 
 # Convert to factor for faster comparison, we're done doing string manipulation
 patDf$patient = as.factor( patDf$patient );
 patDf$place   = as.factor( patDf$place   );
+patDf$date    = as.numeric( patDf$date );
 
-## Create adjacency matrix
-patientMatrix = matrix(0, ncol=length(patients), nrow=length(patients) );
+## Create pseudo-adjacency matrix
+# numbers indicate days
+patientMatrix = matrix(Inf, ncol=length(patients), nrow=length(patients) );
 rownames( patientMatrix ) = patients;
 colnames( patientMatrix ) = patients;
 
@@ -92,31 +93,137 @@ roomMatrix = matrix(0, ncol=length(rooms), nrow=length(rooms) );
 rownames( roomMatrix ) = rooms;
 colnames( roomMatrix ) = rooms;
 
+# Ugh...  This needs to be cleaned up.  The patient x room space isn't as big
+# as the rowxrow space, but still.
+repmat = function(X,m,n){
+  ##R equivalent of repmat (matlab)
+  mx = dim(X)[1]
+  nx = dim(X)[2]
+  matrix(t(matrix(X,mx,nx*n)),mx*m,nx*n,byrow=T)
+}
+
+repmatVec = function(Y,m,n){
+  X = as.matrix(Y);
+  ##R equivalent of repmat (matlab)
+  mx = dim(X)[1]
+  nx = dim(X)[2]
+  matrix(t(matrix(X,mx,nx*n)),mx*m,nx*n,byrow=T)
+}
+
+patAdjList = function(adjList, n) {
+  if( n < 1 ) {
+    return( adjList );
+  } else {
+    thesePats = patientMatrix[ patients %in% adjList, ];
+    if( is.null( nrow( thesePats ) ) ) {
+      theseHits = thesePats > 0;
+    } else {
+      theseHits = apply(thesePats,2,sum) > 0;
+    }
+    return( patAdjList( c(patients[ theseHits ], adjList ), n-1 ) );
+  }
+}
+
+dayFunc = function(X) {
+ Y = X[ X > -1 ];
+ if( length(Y) == 0) {
+   return( NA );
+ } else {
+   return( min(Y) );
+ }
+}
+
 for( thisPatient in patients) {
   hotRooms = unique( patDf$place[ patDf$patient == thisPatient]);
   hotRows  = patDf[ patDf$place %in% hotRooms, ];
 
+  for( thisRoom in hotRooms ) {
+    patDates    = hotRows[ hotRows$patient == thisPatient & hotRows$place == thisRoom, 2];
+    roomSubset  = hotRows[ hotRows$patient != thisPatient & hotRows$place == thisRoom,];
+    roomDates   = roomSubset$date;
 
+    # If either of the dates are zero, then bail out
+    if( length( patDates ) == 0 | length( roomDates ) == 0 ) {
+      next;
+    }
 
-  hotRows  = hotRows[ hotRows$patient != thisPatient, ];
+    # Construct an array of the possible date differences.  This is probably overkill,
+    # but affords flexibility if I want to create rules later
+    # You only care about the row index of hits.  The row tells you which patient it was.
+    # Positive values indicate that thisPatient was in the given room after.
+    timeMat = repmatVec(t(patDates), length(roomDates), 1) - repmatVec(roomDates, 1, length(patDates));
 
+    # NA if patient was not in a given room before thisPatient, number of days otherwise
+    patWeights = apply( timeMat, 1, dayFunc);
+    updateDf = data.frame(days=patWeights, patient=roomSubset$patient );
+
+    # Update the pseudo-adjacency matrix
+    for( thatPatient in updateDf$patient ) {
+
+      # Do this the lazy way; reference updateDf, which may have multiple entries for the same patient.
+      # so choose the min (updateDf ensured to be non-negative).
+      thisDays = min( updateDf[ updateDf$patient == thatPatient, 1 ] );
+
+      if( ! is.na( thisDays ) ) {
+        thatDays = patientMatrix[ colnames( patientMatrix) == thisPatient, colnames( patientMatrix) == thatPatient];
+
+        if( thisDays < thatDays ) {
+          patientMatrix[ colnames( patientMatrix) == thisPatient, colnames( patientMatrix) == thatPatient] = thisDays;
+        }
+      }
+    }
+
+  }
 }
+
+# Finalize matrix
+patientMatrix[ patientMatrix==Inf ] = 0;
 
 # Define server logic
 shinyServer(function(input, output) {
+  #### Overview of server logic ####
+  #
+  # The graphics-facing data structure is an adjacency matrix defined in multiple steps.
+  # Some reference steps are handled above.  From them:
+  #   1.  The user selects a room filter, which is engaged first.  This is expensive as it is done before the pseudo-adjaceny matrix is created
+  #   2.  A patient filter, which is engaged second
+  #   3.  Finally, there are options to convert the pseudo-adjacency matrix to the adjacency matrix in different ways
+
+
 
   # Define patient list
   output$choose_pats = renderUI({
 
     # Create checkboxes; have all ticked by default
-    checkboxGroupInput("thesePats", "Select patients:", patients, patients)
+    selectInput("targetPatients", "Target patient:", choices=patients, selected=patients[1])
+
   });
 
   ### Graph visualization
   output$adjGraph = renderPlot({
-    thisAdj = patientMatrix[ patients %in% input$thesePats, patients %in% input$thesePats ];
+    thesePats = patAdjList( input$targetPatients, input$nDisplay );
+    thisAdj = patientMatrix[ patients %in% thesePats, patients %in% thesePats ];
+    thisAdj[ thisAdj > input$nDays ] = 0;
+    thisAdj[ thisAdj > 0 ] = 1/thisAdj[ thisAdj > 0];
+
+    thisAdj = thisAdj[ apply( thisAdj, 1, sum) > 0, apply( thisAdj, 1, sum) > 0 ];
+
     patientGraph <- graph.adjacency(thisAdj, mode="directed", weighted=TRUE);
-    x = plot.igraph(patientGraph, edge.arrow.size=input$arrowSize, vertex.size = input$vertexSize, vertex.label.cex = input$labelSize);
+#    x = plot.igraph(patientGraph, edge.arrow.size=input$arrowSize, vertex.size = input$vertexSize, vertex.label.cex = input$labelSize);
+
+    thisLayout = layout.kamada.kawai( patientGraph );
+
+    x = plot.igraph(patientGraph,
+                    vertex.label=V(patientGraph)$name,
+                    layout=thisLayout,
+                    vertex.label.color="black",
+                    edge.color="black",
+                    edge.width=E(patientGraph)$weight*input$weightCoeff,
+                    edge.arrow.size=input$arrowSize,
+                    vertex.size = input$vertexSize,
+                    vertex.label.cex = input$labelSize,
+                    margin=c(input$margTop, input$magLeft, input$margBot, input$margRight)
+    );
 
     return(x);
   })
--- ui.r
+++ ui.r
@@ -2,32 +2,80 @@ library(shiny)
 
 # Define UI, based loosely on one of Shiny's templates
 shinyUI(fluidPage(
-  
+
   ### SIDE PANEL ###
   fluidRow(
   column(3,
     titlePanel("Prototype Cdiff graph viewer"),
-    
+
     # Simulation configuration
     tabsetPanel(
       tabPanel("Patients",
-        uiOutput("choose_pats")
+        uiOutput("choose_pats"),
+        sliderInput("nDisplay", "Neighborhood order",
+          min = 1,
+          max = 5,
+          value = 2,
+          step = 1),
+
+        sliderInput("nDays", "Maxium Days",
+          min = 1,
+          max = 10,
+          value = 2,
+          step = 1)
       ),
+
       tabPanel("Viz Parameters",
-        numericInput("arrowSize", "arrow size",
+
+
+        sliderInput("arrowSize", "arrow size",
           min = 0.0,
           max = 5.0,
-          value = 0.1),
-      
-        numericInput("vertexSize", "vertex size",
+          value = 1.0,
+          step = 0.1),
+
+        sliderInput("vertexSize", "vertex size",
+          min = 10,
+          max = 50.0,
+          value = 10,
+          step=0.1),
+
+        sliderInput("labelSize", "label size",
           min = 0.0,
-          max = 5.0,
-          value = 0.1),
-      
-        numericInput("labelSize", "label size",
-          min = 0.0,
-          max = 5.0,
-          value = 0.1)
+          max = 10.0,
+          value = 1.0,
+          step=0.1),
+
+        sliderInput("weightCoeff", "edge weight",
+          min = 0.05,
+          max = 2,
+          value = 0.1,
+          step=0.05),
+
+        sliderInput("margUp", "top margin",
+          min = -1,
+          max = 1,
+          value = 0,
+          step=0.05),
+
+        sliderInput("margLeft", "left margin",
+          min = -1,
+          max = 1,
+          value = 0,
+          step=0.05),
+
+        sliderInput("margBot", "bottom margin",
+          min = -1,
+          max = 1,
+          value = 0,
+          step=0.05),
+
+        sliderInput("MargRight", "right margin",
+          min = -1,
+          max = 1,
+          value = 0,
+          step=0.05)
+
       )
     )
   ),
@@ -37,7 +85,7 @@ shinyUI(fluidPage(
     tabsetPanel(id="MAIN_PANEL",
       tabPanel("Graph Theory",
                "Some quick graphs.",
-       plotOutput("adjGraph")
+       plotOutput(outputId = "adjGraph",  width = "100%")
       )
     )
   )
